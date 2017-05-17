2742af709e80ad2ed6ca4c1cb99a31f19336a42d
zainab-ali <zainab.ali.london@gmail.com>
2017-05-17 19:59:21 +0100

    use apomorphisms

--- src/main/scala/app/BTree.scala
+++ src/main/scala/app/BTree.scala
@@ -25,6 +25,8 @@ case class Leaf[A, B](index: Int, value: A) extends BTree[A, B]
 //can actually annotate this with an interval range
 case class Node[A, B](l: B, r: B) extends BTree[A, B]
 
+case class Zipped[A](l: Option[A], a: A, r: Option[A])
+
 object BTree {
 
   implicit def btreeRecurseFunctor[C]: Functor[BTree[C, ?]] = new Functor[BTree[C, ?]] {
@@ -57,62 +59,34 @@ object BTree {
     case Node(_, r) => r
   }
 
-  def setLeft[A](a: A): Either[Fix[BTree[Zipped[A], ?]], Fix[BTree[Zipped[A], ?]]] => BTree[Zipped[A], Either[Fix[BTree[Zipped[A], ?]], Fix[BTree[Zipped[A], ?]]]] = {
-    case Left(Fix(Leaf(i, z))) => Leaf(i, z.copy(l = Some(a)))
-    case Left(Fix(Node(l, r))) => Node(Left(l), Right(r))
-    case Right(Fix(Leaf(i, z))) => Leaf(i, z)
-    case Right(Fix(Node(l, r))) => Node(Right(l), Right(r))
+  def modifyLeft[A](f: A => A): Fix[BTree[A, ?]] => BTree[A, \/[Fix[BTree[A, ?]], Fix[BTree[A, ?]]]] = {
+    case Fix(Leaf(i, a)) => Leaf(i, f(a))
+    case Fix(Node(l, r)) => Node(\/.right(l), \/.left(r))
   }
 
-
-  def modifyLeft[A](f: A => A): Either[Fix[BTree[A, ?]], Fix[BTree[A, ?]]] => BTree[A, Either[Fix[BTree[A, ?]], Fix[BTree[A, ?]]]] = {
-    case Left(Fix(Leaf(i, z))) => Leaf(i, f(z))
-    case Left(Fix(Node(l, r))) => Node(Left(l), Right(r))
-    case Right(Fix(Leaf(i, z))) => Leaf(i, z)
-    case Right(Fix(Node(l, r))) => Node(Right(l), Right(r))
+  def modifyRight[A](f: A => A): Fix[BTree[A, ?]] => BTree[A, \/[Fix[BTree[A, ?]], Fix[BTree[A, ?]]]] = {
+    case Fix(Leaf(i, a)) => Leaf(i, f(a))
+    case Fix(Node(l, r)) => Node(\/.left(l), \/.right(r))
   }
-
-  def setRight[A](a: A): Either[Fix[BTree[Zipped[A], ?]], Fix[BTree[Zipped[A], ?]]] => BTree[Zipped[A], Either[Fix[BTree[Zipped[A], ?]], Fix[BTree[Zipped[A], ?]]]] = {
-    case Left(Fix(Leaf(i, z))) => Leaf(i, z)
-    case Left(Fix(Node(l, r))) => Node(Left(l), Left(r))
-    case Right(Fix(Leaf(i, z))) => Leaf(i, z.copy(r = Some(a)))
-    case Right(Fix(Node(l, r))) => Node(Left(l), Right(r))
-  }
-
-
-  def modifyRight[A](f: A => A): Either[Fix[BTree[A, ?]], Fix[BTree[A, ?]]] => BTree[A, Either[Fix[BTree[A, ?]], Fix[BTree[A, ?]]]] = {
-    case Left(Fix(Leaf(i, z))) => Leaf(i, z)
-    case Left(Fix(Node(l, r))) => Node(Left(l), Left(r))
-    case Right(Fix(Leaf(i, z))) => Leaf(i, f(z))
-    case Right(Fix(Node(l, r))) => Node(Left(l), Right(r))
-  }
-
-  case class Zipped[A](l: Option[A], a: A, r: Option[A])
 
   //a basic catamorphism
   def zipNeighbours[A]: BTree[A, Fix[BTree[Zipped[A], ?]]] => Fix[BTree[Zipped[A], ?]] = {
     case Leaf(i, a) => Fix(Leaf(i, Zipped(None, a, None)))
-    case Node(l, r) => // we want to set l's right to r and r's left to l
+    case Node(l, r) =>
       val lr = l.cata(right)
       val rl = r.cata(left)
-      println(s"values $lr $rl")
-      val leftr: Either[Fix[BTree[Zipped[A], ?]], Fix[BTree[Zipped[A], ?]]] = Left(r)
-      val rightl: Either[Fix[BTree[Zipped[A], ?]], Fix[BTree[Zipped[A], ?]]] = Right(l)
-      val lana = leftr.ana[Fix[BTree[Zipped[A], ?]]](setLeft(lr.a))
-      val rana = rightl.ana[Fix[BTree[Zipped[A], ?]]](setRight(rl.a))
-      println(s"fixes $lana $rana")
-      Fix(Node[Zipped[A], Fix[BTree[Zipped[A], ?]]](rana, lana))
+      val nextr = r.apo[Fix[BTree[Zipped[A], ?]]](modifyLeft(_.copy(l = Some(lr.a))))
+      val nextl = l.apo[Fix[BTree[Zipped[A], ?]]](modifyRight(_.copy(r = Some(rl.a))))
+      Fix(Node[Zipped[A], Fix[BTree[Zipped[A], ?]]](nextl, nextr))
   }
 
   def rivers: BTree[Water, Fix[BTree[Water, ?]]] => Fix[BTree[Water, ?]] = {
     case Leaf(i, a) => Fix(Leaf(i, a))
-    case Node(l, r) => // we want to set l's right to r and r's left to l
+    case Node(l, r) =>
       val lr = l.cata(right)
       val rl = r.cata(left)
-      val leftr: Either[Fix[BTree[Water, ?]], Fix[BTree[Water, ?]]] = Left(r)
-      val rightl: Either[Fix[BTree[Water, ?]], Fix[BTree[Water, ?]]] = Right(l)
-      val rnext = if(lr.flux == Flux.Right) leftr.ana[Fix[BTree[Water, ?]]](modifyLeft(w => w.copy(volume = w.volume + 1))) else r
-      val lnext = if(rl.flux == Flux.Left) rightl.ana[Fix[BTree[Water, ?]]](modifyRight(w => w.copy(volume = w.volume + 1))) else l
+      val rnext = if(lr.flux == Flux.Right) r.apo[Fix[BTree[Water, ?]]](modifyLeft(w => w.copy(volume = w.volume + 1))) else r
+      val lnext = if(rl.flux == Flux.Left) l.apo[Fix[BTree[Water, ?]]](modifyRight(w => w.copy(volume = w.volume + 1))) else l
       Fix(Node[Water, Fix[BTree[Water, ?]]](lnext, rnext))
   }
 }
@@ -129,10 +103,10 @@ object Playground extends App {
 
   val random = new Random(123)
 
-  def calcFlux(z: BTree.Zipped[Int]): Flux = z match {
-    case BTree.Zipped(Some(l), a, Some(r)) => if(l <= r && l < a) Flux.Left else if(r < l && r < a) Flux.Right else Flux.Still
-    case BTree.Zipped(Some(l), a, None) => if(l < a) Flux.Left else Flux.Still
-    case BTree.Zipped(None, a, Some(r)) => if(r < a) Flux.Right else Flux.Still
+  def calcFlux(z: Zipped[Int]): Flux = z match {
+    case Zipped(Some(l), a, Some(r)) => if(l <= r && l < a) Flux.Left else if(r < l && r < a) Flux.Right else Flux.Still
+    case Zipped(Some(l), a, None) => if(l < a) Flux.Left else Flux.Still
+    case Zipped(None, a, Some(r)) => if(r < a) Flux.Right else Flux.Still
     case _ => Flux.Still
   }
 
@@ -141,8 +115,8 @@ object Playground extends App {
   val result = (0, 1).ana[Fix[BTree[Int, ?]]](BTree.build(2)(_ => random.nextInt(100) ))
   println(result)
 
-  val zipped: Fix[BTree[BTree.Zipped[Int], ?]] = result.cata(BTree.zipNeighbours)
-  val flux = zipped.cata(BTree.mapValues[BTree.Zipped[Int], Flux, Fix](calcFlux))
+  val zipped: Fix[BTree[Zipped[Int], ?]] = result.cata(BTree.zipNeighbours)
+  val flux = zipped.cata(BTree.mapValues[Zipped[Int], Flux, Fix](calcFlux))
   val water0 = flux.cata(BTree.mapValues[Flux, Water, Fix](startWater))
   val water1 = water0.cata(BTree.rivers)
 
